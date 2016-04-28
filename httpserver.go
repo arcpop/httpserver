f7a5e3fa9fdc493dee02f1ae23e3416c57f44945
rafmeyer <raffaele.meyer@students.fhnw.ch>
2016-04-28 10:31:04 +0200

    first commit

---
+++ httpserver.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "net/http"
+    "fmt"
+	"time"
+)
+
+var response = "<html><head><title>Test</title></head><body><p><h2>Page</h2></p></body></html>"
+
+type CustomHandler struct {
+    
+}
+
+func (c *CustomHandler) ServeHTTP(w http.ResponseWriter, r *http.Request)  {
+    fmt.Fprint(w, response)
+}
+
+
+func main() {
+    s := &http.Server{
+        Addr: ":8080",
+        Handler: &CustomHandler{},
+        ReadTimeout: 5 * time.Second,
+        WriteTimeout: 5 * time.Second,
+        MaxHeaderBytes: 2048,
+    }
+    s.ListenAndServe()
+}
