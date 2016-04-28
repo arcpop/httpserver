45d01fa1397a1768e604d6812a6b9deeb54cee68
rafmeyer <raffaele.meyer@students.fhnw.ch>
2016-04-28 10:36:34 +0200

    Fixed addresses

--- httpserver.go
+++ httpserver.go
@@ -6,6 +6,8 @@ import (
 	"time"
 )
 
+var serverAddresses = []string{"192.168.200.20:8080","192.168.200.30:8080"}
+
 var response = "<html><head><title>Test</title></head><body><p><h2>Page</h2></p></body></html>"
 
 type CustomHandler struct {
@@ -18,12 +20,20 @@ func (c *CustomHandler) ServeHTTP(w http.ResponseWriter, r *http.Request)  {
 
 
 func main() {
-    s := &http.Server{
-        Addr: ":8080",
+    s1 := &http.Server{
+        Addr: serverAddresses[0],
         Handler: &CustomHandler{},
         ReadTimeout: 5 * time.Second,
         WriteTimeout: 5 * time.Second,
         MaxHeaderBytes: 2048,
     }
-    s.ListenAndServe()
+    go s1.ListenAndServe()
+    s2 := &http.Server{
+        Addr: serverAddresses[1],
+        Handler: &CustomHandler{},
+        ReadTimeout: 5 * time.Second,
+        WriteTimeout: 5 * time.Second,
+        MaxHeaderBytes: 2048,
+    }
+    s2.ListenAndServe()
 }
