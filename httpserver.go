dd4e1698bc32e75196d559b4c94c8b28bf0054e8
rafmeyer <raffaele.meyer@students.fhnw.ch>
2016-04-28 10:39:40 +0200

    first commit

--- httpserver.go
+++ httpserver.go
@@ -6,8 +6,6 @@ import (
 	"time"
 )
 
-var serverAddresses = []string{"192.168.200.20:8080","192.168.200.30:8080"}
-
 var response = "<html><head><title>Test</title></head><body><p><h2>Page</h2></p></body></html>"
 
 type CustomHandler struct {
@@ -20,20 +18,12 @@ func (c *CustomHandler) ServeHTTP(w http.ResponseWriter, r *http.Request)  {
 
 
 func main() {
-    s1 := &http.Server{
-        Addr: serverAddresses[0],
+    s := &http.Server{
+        Addr: ":http",
         Handler: &CustomHandler{},
         ReadTimeout: 5 * time.Second,
         WriteTimeout: 5 * time.Second,
         MaxHeaderBytes: 2048,
     }
-    go s1.ListenAndServe()
-    s2 := &http.Server{
-        Addr: serverAddresses[1],
-        Handler: &CustomHandler{},
-        ReadTimeout: 5 * time.Second,
-        WriteTimeout: 5 * time.Second,
-        MaxHeaderBytes: 2048,
-    }
-    s2.ListenAndServe()
+    s.ListenAndServe()
 }
